7e1f471ffeef548c190327cbf7d8650c1e65815b
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-11-13 14:29:15 -0600

    Udemy-Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,28 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-    }
+        int myValue = 10000;
 
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        byte myMinByteValue = Byte.MIN_VALUE;
+        System.out.println("Byte Minimum Value = " + myMaxByteValue);
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+
+        long myLongValue = 100L;
+        long myMaxLongValue = Long.MAX_VALUE;
+        long myMinLongValue = Long.MIN_VALUE;
+        System.out.println("Long Minimum Value = " + myMaxLongValue);
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        
+        
+    }
 }
